16ff6c1a5d3559923472bfde46f7324b2d24195e
afsalva1 <42220161+afsalva1@users.noreply.github.com>
2020-09-25 20:56:35 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group1" "test" {
-  name     = "testResourceGroup1"
+resource "azurerm_resource_group" "test" {
+  name     = "testResourceGroup12"
   location = var.location
   tags = {
     "Afsal" = "Prod"
