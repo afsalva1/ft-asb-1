95f8428f271699d9134020e0b60c92d121fc883b
afsalva1 <42220161+afsalva1@users.noreply.github.com>
2020-09-25 20:30:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   version = "2.12.0"
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id = "a6948dc4-0d89-49e8-a0ee-b360b78a76f6"
+  client_id       = "e4b5fd9c-e2dd-4f52-9eb1-72d7e7663f69"
+  client_secret   = "JHi_cJT12ln.bNSoK2P5MfBx9fecNU_nzU"
+  tenant_id       = "635a1b40-bd83-4b4f-85fa-8968cf1c8598"
   features {}
 }
